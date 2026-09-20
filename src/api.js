92f74e2e9d706a3445e18b79a5d884182320dbd1
Özlem Sertkaya <ozlemsrtkyy0@gmail.com>
2026-09-20 21:26:35 +0300

    update baseURL to render

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: 'https://campus-ticket-api.onrender.com/api',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
